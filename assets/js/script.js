adb8d56e2458cbdef7ad6297e4d4446b573881f2
sourabh9299 <sourabhkumar987@hotmail.com>
2022-12-14 10:45:49 +0530

    Done

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+
+
+    $(".single-item").slick({
+        infinite: true,
+    autoplay: true,
+    dots: true,
+        });
+
+
+    var testim = document.getElementById("testim"),
+    testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+    touchStartPos,
+    touchEndPos,
+    touchPosDiff,
+    ignoreTouch = 30;
+    ;
+
+
+
+   
+
+
+
+
+
+    // 2nd testimonial =============================================
+
+
+
+
+
+
+    // typed js ================================
+
+    
+
+
+
+
+
