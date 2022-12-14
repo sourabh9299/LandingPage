3d62390f7f476bc2a64e1a7df426cf494da45bfe
sourabh9299 <sourabhkumar987@hotmail.com>
2022-12-14 23:26:10 +0530

    donree

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,27 @@ let typed2 = new Typed('#typed2', {
 });
 
 
-$(".single-item").slick({
+$(".single-item1").slick({
+    speed: 400,
     infinite: true,
     autoplay: true,
     dots: true,
+    autoplaySpeed: 2500
+
 });
+
+$(".single-item2").slick({
+    speed: 400,
+    infinite: true,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 1500
+
+});
+
+
+
+
+
+
+
