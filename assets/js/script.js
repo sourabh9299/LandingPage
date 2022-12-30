bb1e8065fff4721fbb2d03a337e02e71439c85f1
sourabh9299 <sourabhkumar987@hotmail.com>
2022-12-30 14:06:04 +0530

    Changes in Css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,6 @@
-let typed2 = new Typed('#typed2', {
-    strings: ["to learn just the topics of their choice ", "who love live interactions with experts", "who have a busy schedule and prefer flexibility", "who like variety on curriculum choices", "who want to keep with the career trend", "love affordable courses", "love free classes ", "for kids & adults"],
-    typeSpeed: 0,
-    backSpeed: 0,
-    fadeOut: true,
-    loop: true,
-    // loopCount: Infinity
-});
+
+
+
 
 
 $(".single-item1").slick({
